90ea90b58326d2cfc6882698af9edcea6b93103b
ziwuli <lichenxi.zw@gmail.com>
2022-11-03 09:19:43 +0800

    kafka demo

---
+++ kafka/src/main/java/com/ziwu/kafka/KafkaProducerTest.java
@@ -0,0 +1,24 @@
+package com.ziwu.kafka;
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class KafkaProducerTest {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("linger.ms", 5);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<>("my-topic", Integer.toString(i), Integer.toString(i)));
+
+        producer.close();
+    }
+}
